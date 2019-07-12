20a70d4a6aeeb58a32fe5172d792439d808c6a76
Konstantin <admin@mail.ru>
2019-07-12 22:36:20 +0600

    Это наш первый комит

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Grid from 'react-bootstrap/Grid'
+import styled from 'styled-components'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+          <SayFullName name="Константин" surname="Сивостров" link="vk.com" />
+          <SayFullName name="Анна" surname="Карпова" link="ok.com" />
+          <SayFullName name="Марина" surname="Сивострова" link="facebook.com" />
+      </div>
+    )
+  }
 }
+
+
+function SayFullName(props) {
+  return (
+      <>
+      <h1>Мое имя {props.name}, фамилия {props.surname}</h1>
+      <a href={props.link}>Ссылка на мой профиль</a>
+      </>
+  )
+}
+
 
 export default App;
